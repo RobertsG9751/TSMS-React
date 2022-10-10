1382d670d33cc7a8fff850d4967ae0dfe59da81a
RobertsG9751 <r.gutmanis9751@gmail.com>
2022-10-11 02:06:01 +0300

    remove add btn

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -2,7 +2,7 @@ import style from './Button.module.css'
 
 const Button = props =>{
     return(
-        <input className={`${style.button} general_btn`} value={props.value} onClick={props.onClick} type={props.type || "button"}></input>
+        <input className={`${style.button} general_btn addBtn`} value={props.value} onClick={props.onClick} type={props.type || "button"}></input>
     )
 }
 export default Button
