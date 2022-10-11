4afa897f4a5c546bb3cbfc594e8db2658b1e05da
RobertsG9751 <r.gutmanis9751@gmail.com>
2022-10-12 02:31:10 +0300

    detect retina

--- src/Objects/Map.js
+++ src/Objects/Map.js
@@ -35,6 +35,7 @@ const Map = props => {
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                detectRetina="true"
             />
 
             {
