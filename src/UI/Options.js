9d8668950d847dec58dfe6da0f12b1d687847852
RobertsG9751 <r.gutmanis9751@gmail.com>
2022-10-12 02:01:13 +0300

    filtri

--- src/UI/Options.js
+++ src/UI/Options.js
@@ -8,8 +8,32 @@ const Options = props => {
     }
 
     return(
-        <div id="options" className={`${style.box} box_hidden`}>
+        <div id="options" className={`${style.box}`}>
             <button className={style.btn} onClick={closeOptions}>Close</button>
+            <div className={style.options}>
+                <h2>Filtrēt:</h2>
+                <div className={style.box_check}>
+                    <input type="checkbox"></input>
+                    <span> - Vārti</span>
+                </div>
+                <div className={style.box_check}>
+                    <input type="checkbox"></input> 
+                    <span> - Kontrolieri</span>
+                </div>
+                <div className={style.box_check}>
+                <input type="checkbox"></input>
+                    <span> - Skaititājs</span>
+                </div>
+                <div className={style.box_check}>
+                    <input type="checkbox"></input>
+                    <span> - Balsti</span>
+                </div>
+                <div className={style.box_check}>
+                <input type="checkbox"></input>
+                    <span> - Konsole</span>
+                </div>
+                <input className={style.btn} type="button" value="Filtrtēt"></input>
+            </div>
         </div>
     )
 }
